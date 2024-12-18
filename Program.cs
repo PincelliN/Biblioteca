14e5c856f7b95869675487c80f1693ed6094162e
Nicolò Pincelli <pincelli.n@gmail.com>
2024-12-18 11:00:28 +0100

    create class User

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+class User
+{
+    private string id;
+    private string name;
+    private string lastname;
+    private int yearOfRegistration;
+
+    public User(string id, string name, string lastname, int yearOfRegistration)
+    {
+        this.id = id;
+        this.name = name;
+        this.lastname = lastname;
+        this.yearOfRegistration = yearOfRegistration;
+    }
+
+    public string denomination(string name, string lastname)
+    {
+        string result = $"{name} {lastname}";
+        return result;
+    }
+}
