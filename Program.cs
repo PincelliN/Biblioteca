596ef7037a4b4b0b93df69aa85aa2f2f17b3469c
Nicolò Pincelli <pincelli.n@gmail.com>
2024-12-18 12:03:45 +0100

    create Load and Refund methods

--- Program.cs
+++ Program.cs
@@ -1,49 +1,71 @@
 ﻿// See https://aka.ms/new-console-template for more information
-public class Utente
+
+namespace BibloLibrary
 {
-    private string id;
-    private string name;
-    private string lastname;
-    private int yearOfRegistration;
-
-    public Utente(string id, string name, string lastname, int yearOfRegistration)
+    public class Utente
     {
-        this.id = id;
-        this.name = name;
-        this.lastname = lastname;
-        this.yearOfRegistration = yearOfRegistration;
+        private string id;
+        private string name;
+        private string lastname;
+        private int yearOfRegistration;
+
+        public Utente(string id, string name, string lastname, int yearOfRegistration)
+        {
+            this.id = id;
+            this.name = name;
+            this.lastname = lastname;
+            this.yearOfRegistration = yearOfRegistration;
+        }
+
+        public string Denomination()
+        {
+            return this.name + " " + this.lastname;
+        }
     }
 
-    public string denomination(string name, string lastname)
+    public class Book
     {
-        string result = $"{name} {lastname}";
-        return result;
+        private string id;
+        private string title;
+        private string author;
+        //oggetto della classe utente 
+        private Utente utente;
+
+
+        public Book(string id, string title, string author)
+        {
+            this.id = id;
+            this.title = title;
+            this.author = author;
+            this.utente = null;
+        }
+        public string Description()
+        {
+            return this.title + " di " + this.author;
+        }
+
+        public void Loan(Utente utente)
+        {
+            if (this.utente != null)
+            {
+                Console.WriteLine($"{this.title} è gia in prestito a {this.utente.Denomination()}");
+                return;
+            }
+            else
+            {
+                this.utente = utente;
+                Console.WriteLine($"{this.title} è stato prestato a {this.utente.Denomination()}");
+            }
+
+        }
+        public void Refund()
+        {
+            Console.WriteLine($"{this.title} è stato restituito da {this.utente.Denomination()}");
+            this.utente = null;
+
+        }
+
+
     }
+
 }
-
-public class Book
-{
-    private string id;
-    private string title;
-    private string author;
-    private string Utente;
-
-    public Book(string id, string title, string author, string Utente)
-    {
-        this.id = id;
-        this.title = title;
-        this.author = author;
-        this.Utente = Utente.denomination;
-    }
-
-    public Book(string id, string title, string author)
-    {
-        this.id = id;
-        this.title = title;
-        this.author = author;
-        this.Utente = null;
-    }
-
-
-}
-
